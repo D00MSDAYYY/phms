c51861c353660de5308b57b98b5827186bf447e3
hashiramasenju <>
2023-11-02 18:09:48 +0300

    решение дз

--- ДЗ /2.11.2023/main.cpp
+++ ДЗ /2.11.2023/main.cpp
@@ -1,6 +1,82 @@
 
+#include <iostream>
+//! заменить int choice на enum CHOICE
+enum CHOICE
+{
+	first,
+	second,
+	third,
+	fourth
+};
+namespace HOMEWORK
+{
+	bool selector(CHOICE choice, int message);
+	void first(int m);
+	void second(int m);
+	void third(int m);
+	void fourth(int m);
+}
+
+void HOMEWORK::first(int m)
+{
+	std::cout << "<" << m << ">";
+}
+void HOMEWORK::second(int m)
+{
+	std::cout << '"' << m << '"';
+}
+void HOMEWORK::third(int m)
+{
+	std::cout << '(' << m << ')';
+}
+void HOMEWORK::fourth(int m)
+{
+	std::cout << '{' << m << '}';
+}
+
+bool HOMEWORK::selector(CHOICE choice, int message)
+{
+	switch (choice)
+	{
+	case 0:
+		HOMEWORK::first(message);
+		break;
+	case 1:
+		HOMEWORK::second(message);
+		break;
+	case 2:
+		HOMEWORK::third(message);
+		break;
+	case 3:
+		HOMEWORK::fourth(message);
+		break;
+	default:
+		std::cout << "ERROR";
+		break;
+	}
+
+	if (message >= 0 && message <= 150)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
 int main()
 {
+	std::cout << HOMEWORK::selector(CHOICE::first, 100); // true
+	std::cout << std::endl;
+	std::cout << HOMEWORK::selector(CHOICE::second, 0);
+	std::cout << std::endl;
+	std::cout << HOMEWORK::selector(CHOICE::third, 99999);
+	std::cout << std::endl;
+	std::cout << HOMEWORK::selector(CHOICE::fourth, 42);
+	std::cout << std::endl;
+	std::cout << HOMEWORK::selector(CHOICE::first, 200); // true
+	std::cout << std::endl;
+	return 0;
 	//! в пространстве имен HOMEWORK объявить 5 функций:
 	//! bool selector(int option, int message)
 	//! void first(int m)
@@ -22,7 +98,7 @@ int main()
 	//! если choice не подходит не под одно условие, то вывести сообщение "ERROR"
 	//! если значение message не находится в диапазоне от 0 до 150, вернуть значени true, иначе - false
 
-	//! в функции main() вызвать функцию selector c различными значениями 
+	//! в функции main() вызвать функцию selector c различными значениями
 
 	//! заменить int choice на enum CHOICE
 }
--- ДЗ /2.11.2023/main2.cpp
+++ ДЗ /2.11.2023/main2.cpp
@@ -1,7 +1,140 @@
+#include <iostream>
 
-
-int maint()
+namespace PHMS
 {
+	namespace INFORMATICS
+	{
+		enum EDU_TYPE
+		{
+			SCHOOL,
+			LICEUM,
+			HOMESTUD,
+			OTHER
+		};
+
+		struct student
+		{
+			int ID;
+			float height;
+			float weight;
+			float avr_mark;
+			EDU_TYPE edu_type;
+			int friend_ID;
+		};
+
+		bool makeFriends(student s1, student s2);
+		bool unmakeFriends(student s1, student s2);
+		void showInfo(student s1);
+		student shuffleInfo(student s1);
+	}
+}
+
+bool PHMS::INFORMATICS::makeFriends(student s1, student s2)
+{
+	if (s1.friend_ID == s2.ID)
+	{
+		return false;
+	}
+	else
+	{
+		s1.friend_ID = s2.ID;
+		s2.friend_ID = s1.ID;
+		return true;
+	}
+}
+bool PHMS::INFORMATICS::unmakeFriends(student s1, student s2)
+{
+	// showInfo(s1);
+	// showInfo(s2);
+	if (s1.friend_ID != s2.ID)
+	{
+		return false;
+	}
+	else
+	{
+		s1.friend_ID = s1.ID;
+		s2.friend_ID = s2.ID;
+		showInfo(s1);
+		showInfo(s2);
+		return true;
+	}
+}
+void PHMS::INFORMATICS::showInfo(student s1)
+{
+	std::cout << "ID " << s1.ID << std::endl
+			  << "height " << s1.height << std::endl
+
+			  << "weight " << s1.weight << std::endl
+			  << "avr_mark " << s1.avr_mark << std::endl
+			  << "friend_ID " << s1.friend_ID << std::endl;
+	std::cout << "edu_type ";
+
+	switch (s1.edu_type)
+	{
+	case SCHOOL:
+		std::cout << "school";
+		break;
+	case LICEUM:
+		std::cout << "liceum";
+		break;
+	case HOMESTUD:
+		std::cout << "homestud";
+		break;
+	case OTHER:
+		std::cout << "other";
+		break;
+	default:
+		std::cout << "no data";
+		break;
+	}
+	std::cout << std::endl
+			  << "---------------------------------------" 
+			  << std::endl;
+}
+PHMS::INFORMATICS::student PHMS::INFORMATICS::shuffleInfo(student s1)
+{
+
+	int tmp1 = s1.ID;
+	float tmp2 = s1.height;
+	float tmp3 = s1.weight;
+	float tmp4 = s1.avr_mark;
+	int tmp5 = s1.friend_ID;
+	EDU_TYPE tmp6 = s1.edu_type;
+
+	s1.ID = tmp6;
+	s1.height = tmp5;
+	s1.weight = tmp4;
+	s1.avr_mark = tmp3;
+	s1.friend_ID = tmp2;
+	s1.edu_type = EDU_TYPE(tmp1);
+
+	return s1;
+}
+
+int main()
+{
+	PHMS::INFORMATICS::student ivan{
+		123,
+		170.8,
+		60.9,
+		4.5,
+		PHMS::INFORMATICS::EDU_TYPE::HOMESTUD,
+		123};
+
+	PHMS::INFORMATICS::student sasha{
+		123,
+		170.8,
+		60.9,
+		4.5,
+		PHMS::INFORMATICS::EDU_TYPE::HOMESTUD,
+		123};
+
+	PHMS::INFORMATICS::showInfo(ivan);
+	PHMS::INFORMATICS::showInfo(PHMS::INFORMATICS::shuffleInfo(ivan));
+
+	PHMS::INFORMATICS::showInfo(sasha);
+	PHMS::INFORMATICS::showInfo(PHMS::INFORMATICS::shuffleInfo(sasha));
+
 	//! в пространстве имен PHMS создать пространство имен INFORMATICS
 
 	//! в пространстве имен PHMS::INFORMATICS:
@@ -37,6 +170,6 @@ int maint()
 	//! 5) определить функцию student shuffleInfo(student s1):
 	//! ID, рост, вес, средний бал должны быть перемешаны
 
-	//! в функции main() создать несколько произвольных переменных с типом PHMS::INFORMATICS::student 
+	//! в функции main() создать несколько произвольных переменных с типом PHMS::INFORMATICS::student
 	//! продемонстрировать работу всех функций
 }

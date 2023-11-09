293ad12ccacaceddac4134ca9bd9cec19cdb9b99
hashiramasenju <>
2023-11-09 22:02:02 +0300

    add lessons code

---
+++ lessons_code/code2.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+void increase(int a);
+
+void increase(int *a);
+
+int main()
+{
+	// break;
+	// continue;
+
+	// int arr[10] = {1, 2, 333, 4, 5, 6};
+
+	// int result{0};
+
+	// for (int i{0}; i < 10; ++i)
+	// {
+	// 	std::cerr << arr[i] << " ";
+	// 	if (arr[i] == 333)
+	// 	{
+	// 		result = i;
+	// 		// break;
+	// 	}
+	// }
+
+	// float qwerty[20] = {0,
+	// 					1,
+	// 					0,
+	// 					2,
+	// 					5,
+	// 					90,
+	// 					0,
+	// 					1,
+	// 					11,
+	// 					0};
+
+	// float var = 1000;
+
+	// for (int m{0}; m < 20; ++m)
+	// {
+	// 	if(qwerty[m] == 0)
+	// 	{
+	// 		continue;
+	// 	}
+	// 	std::cout << var / qwerty[m] << std::endl;
+	// }
+
+	// ! дан массив чисел с целым типом данных, размером 100 элементов
+	// ! найти первый четный элемент , вернуть его индекс
+	// ! если в массиве присутствует число 13,
+	// ! завершить выполнение программы, вывести сообщение "ошибка"
+
+	// int a{7};
+
+	// int *  a_ptr;
+	// int *b_ptr;
+
+	// a_ptr = &a;
+
+	// int b{88};
+
+	// b_ptr = &b;
+
+	// std::cout << a_ptr << " " << b_ptr;
+
+	// int my_int{99};
+
+	// increase(&my_int); // a = my_int
+
+	// std::cout << my_int;
+
+	// int a;
+	// a = 7;
+
+	// int* a_ptr;
+	// a_ptr = &a;
+
+	// a_ptr++;
+	// (*a_ptr)++;
+
+//! создать функции:
+//! функция void swaper(int* a, int* b, int* c)
+
+//!
+// (int* c)
+// {
+// 	int c = *a;
+// }
+
+// 	int a = 6;
+// 	float a = 10.6;
+
+// 	std::cout << a;
+}
--- ДЗ /9.11.2023/main.cpp
+++ ДЗ /9.11.2023/main.cpp
@@ -11,4 +11,55 @@ int main()
 	//! после этого должно выводится общее колличество символов в строке,
 	//! колличество ру букв, колличество англ букв
 	//! колличество заглавны букв, колличество строчных букв
+
+	char str[30] = "saasfsAAAafsda";
+
+	bool rus_flag{false};
+	bool eng_flag{false};
+
+	int count_all{0};
+	int count_rus{0};
+	int count_eng{0};
+
+	int count_capital{0};
+	int count_small{0};
+
+	bool end_reached{false};
+
+	for (int i{0}; i < 30; ++i)
+	{
+		if ('а' <= str[i] && str[i] <= 'я')
+		{
+			rus_flag = true;
+			++
+			++count_rus;
+		}
+		if ('a' <= str[i] && str[i] <= 'z')
+		{
+			eng_flag = true;
+			++count_eng;
+		}
+		if ('А' <= str[i] && str[i] <= 'Я')
+		{
+			rus_flag = true;
+			++count_capital;
+			++count_rus;
+		}
+		if ('A' <= str[i] && str[i] <= 'Z')
+		{
+			eng_flag = true;
+			++count_small;
+			++count_eng;
+		}
+		if(str[i] == '\0')
+		{
+			end_reached = true;
+		}
+		if(!end_reached)
+		{
+			++count_all;
+		}
+	}
+
+	
 }

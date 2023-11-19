4f65ca292610413b09ea49292526f3007ea89a08
hashiramasenju <>
2023-11-19 14:43:15 +0300

    дз на 21

---
+++ lessons_code/code3.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+void print(char ch = 'y', int num = 5, char prefix = '/')
+{
+	std::cerr << prefix << " ";
+	for (int i{0}; i < num; ++i)
+	{
+		std::cerr << ch << " ";
+	}
+	std::cerr << prefix;
+	std::cerr << std::endl;
+}
+
+void print(char *str, int num)
+{
+	int i{0};
+	while (num)
+	{
+		std::cerr << *(str + i);
+		++i;
+		--num;
+	}
+	std::cerr  << std::endl;
+}
+
+void print(double a, double b)
+{
+	std::cerr << a + b << std::endl;
+}
+
+int main()
+{
+	print('q', 3, '-');
+
+	char str[15] = "hello world";
+	print(&(str[0]), 5);
+
+	print(2.35, 1.00003);
+
+	//! создать 3 функции func()
+	//! 1 : принимает целочисленные аргументы и выводит их через запятую
+	//! 2 : принимает указатели на double и выводит их через точку с запятой
+	//! 3 : не принимает никаких аргументов и выводит черту "---------------"
+
+	// print('q', 3, '-');
+
+	// print('a', 4);
+
+	// print('w');
+
+	// print();
+
+	//! определить функцию divider(int a, int b)
+	//! она должна иметь один аргумент по умолчанию, равный 4
+	//! она должна делить a на b и выводить результат
+
+
+
+
+
+
+}
---
+++ lessons_code/code4.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+// enum LEAGUE
+// {
+// 	High,
+// 	Medium,
+// 	Low
+// };
+
+// struct Team
+// {
+// 	int num_of_players;
+// 	char *name;
+// 	LEAGUE league;
+// 	double score;
+// };
+
+// void showInfo(Team t);
+// void deleteTeam(Team t);
+// void changeName(Team t);
+// // change ...
+
+// int main()
+// {
+// 	Team t1{10, "name", LEAGUE::Low, 1.0};
+// 	t1.num_of_players = -1;
+// 	t1.score = -12;
+
+// 	showInfo(t1);
+// }
+
+// class Team
+// {
+// private:
+// 	int num_of_players;
+// 	char* name;
+// 	LEAGUE league;
+// 	double score;
+
+// public:
+// 	void showInfo();
+// 	void deleteTeam();
+// 	void changeName(char *str);
+
+// 	void changeScore(double d)
+// 	{
+// 		if (score + d < 0)
+// 			return;
+// 		else
+// 			score += d;
+// 	}
+// };
+
+// int main()
+// {
+// 	Team t2{10, "name", LEAGUE::Low, 1.0};
+
+// 	t2.changeScore(-100);
+
+// 	t2.changeScore(5);
+
+// 	t2.changeName("maxim");
+// }
+
+//! создать класс Банковский счет
+
+//! класс должен содержать в себе приватные поля :
+//! id
+//! имя
+//! возраст
+//! город
+//! гендер(енум)
+//! колличество лет существования счета
+
+//! класс должен содержать в себе публичные поля :
+//! изменить  ... (каждое поля в отдельности)
+
+// enum GENDER
+// {
+// 	Male,
+// 	Female
+// };
+
+// class Bank_Account
+// {
+// private:
+// 	int _ID{};
+// 	char _name[90];
+// 	int _age{};
+// 	char _city[90];
+// 	GENDER _gender{};
+// 	int _duration{};
+
+// public:
+// 	void changeID(int ID)
+// 	{
+// 		if (ID > 0 && ID < 1000)
+// 			_ID = ID;
+// 	}
+
+// 	int getID()
+// 	{
+// 		return _ID;
+// 	}
+// };
+
+// int main()
+// {
+// 	Bank_Account acc1;
+// 	acc1.changeID(100);
+// 	std::cerr << acc1.getID() << " ";
+// 	acc1.changeID(1000000);
+// 	std::cerr << acc1.getID() << " ";
+// }
